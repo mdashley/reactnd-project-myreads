6219a18d2370b3f3a6e051809c078106df3f9fce
Michael Ashley <michaelashley708@gmail.com>
2019-07-10 11:23:21 -0400

    Render books from BooksAPI

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import * as BooksAPI from '../BooksAPI';
 import Book from '../components/Book';
 
 class MainPage extends Component {
+  state = {
+    books: [],
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(booksArr => this.setState({ books: booksArr }));
+  }
+
   render() {
     return (
       <div className="main-page-container">
@@ -15,20 +24,19 @@ class MainPage extends Component {
                 <h2 className="bookshelf-title">Currently Reading</h2>
                 <div className="bookshelf-books">
                   <ol className="books-grid">
-                    <li>
-                      <Book
-                        title="To Kill a Mockingbird"
-                        cover="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
-                        author="Harper Lee"
-                      />
-                    </li>
-                    <li>
-                      <Book
-                        title="Ender's Game"
-                        cover="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-                        author="Orson Scott Card"
-                      />
-                    </li>
+                    {this.state.books
+                      .filter(book => book.shelf === 'currentlyReading')
+                      .map(book => {
+                        return (
+                          <li key={book.id}>
+                            <Book
+                              title={book.title}
+                              cover={book.imageLinks.thumbnail}
+                              author={book.authors[0]}
+                            />
+                          </li>
+                        );
+                      })}
                   </ol>
                 </div>
               </div>
@@ -36,20 +44,19 @@ class MainPage extends Component {
                 <h2 className="bookshelf-title">Want to Read</h2>
                 <div className="bookshelf-books">
                   <ol className="books-grid">
-                    <li>
-                      <Book
-                        title="1776"
-                        cover="http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-                        author="David McCullough"
-                      />
-                    </li>
-                    <li>
-                      <Book
-                        title="Harry Potter and the Sorcerer's Stone"
-                        cover="http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-                        author="J.K. Rowling"
-                      />
-                    </li>
+                    {this.state.books
+                      .filter(book => book.shelf === 'wantToRead')
+                      .map(book => {
+                        return (
+                          <li key={book.id}>
+                            <Book
+                              title={book.title}
+                              cover={book.imageLinks.thumbnail}
+                              author={book.authors[0]}
+                            />
+                          </li>
+                        );
+                      })}
                   </ol>
                 </div>
               </div>
@@ -57,27 +64,19 @@ class MainPage extends Component {
                 <h2 className="bookshelf-title">Read</h2>
                 <div className="bookshelf-books">
                   <ol className="books-grid">
-                    <li>
-                      <Book
-                        title="The Hobbit"
-                        cover="http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
-                        author="J.R.R. Tolkien"
-                      />
-                    </li>
-                    <li>
-                      <Book
-                        title="Oh, the Places You'll Go!"
-                        cover="http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
-                        author="Seuss"
-                      />
-                    </li>
-                    <li>
-                      <Book
-                        title="The Adventures of Tom Sawyer"
-                        cover="http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"
-                        author="Mark Twain"
-                      />
-                    </li>
+                    {this.state.books
+                      .filter(book => book.shelf === 'read')
+                      .map(book => {
+                        return (
+                          <li key={book.id}>
+                            <Book
+                              title={book.title}
+                              cover={book.imageLinks.thumbnail}
+                              author={book.authors[0]}
+                            />
+                          </li>
+                        );
+                      })}
                   </ol>
                 </div>
               </div>
